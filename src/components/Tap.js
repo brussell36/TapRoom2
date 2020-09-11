9ac2d9a9801b83ae28a475404ef8fc490079a760
Ben Russell <benjaminrussell36@gmail.com>
2020-09-11 09:05:02 -0700

    Create Tap.js component

---
+++ src/components/Tap.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Tap(props){
+  return(
+    <React.Fragment>
+      <div onClick= {() => props.whenTicketClicked(props.id)}>
+        <h3>{props.name} - {props.brand}</h3>
+        <p>{props.price} - {props.alcoholContent}</p>
+        <hr/>
+      </div>
+    </React.Fragment>
+  )
+}
+
+Tap.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.string,
+  alcoholContent: PropTypes.string,
+  whenTapClicked: PropTypes.func
+};
+
+export default Tap;
