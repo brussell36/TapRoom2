22b0f7f9bf17888564d4f7997d4cecc5be02e7bb
Ben Russell <benjaminrussell36@gmail.com>
2020-09-11 10:02:57 -0700

    Add handleAddingNewTapToList method

--- src/components/TapControl.js
+++ src/components/TapControl.js
@@ -31,5 +31,13 @@ class TapControl extends React.Component{
     }
   }
 
+  handleAddingNewTapToList = (newTap) => {
+    const newMasterTapList = this.state.masterTapList.concat(newTap);
+    this.setState({
+      masterTapList: newMasterTapList,
+      formVisibleOnPage: false
+    });
+  }
+
   
 }
