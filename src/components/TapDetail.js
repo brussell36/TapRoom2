906367c3bbbd63e6aa1acd0bb01db8b4282c720e
Ben Russell <benjaminrussell36@gmail.com>
2020-09-11 11:23:50 -0700

    Add TapDetail.js and logic

---
+++ src/components/TapDetail.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "react-bootstrap/Button";
+
+function TapDetail(props){
+  const { tap, onClickingDelete } = props;
+
+  return (
+    <React.Fragment>
+      <h1>Tap Detail</h1>
+      <h3>{tap.name} - {tap.brand}</h3>
+      <p>{tap.price} - {tap.alcoholContent}</p>
+      <Button style={{margin: 10}} variant="success" onClick={ props.onClickingEdit }>Update Tap</Button>
+      <Button variant="danger" onClick={() => onClickingDelete(tap.id) }>Delete Tap</Button>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+TapDetail.propTypes = {
+  tap: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
+};
+
+export default TapDetail;
