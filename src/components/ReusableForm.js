e75256f1dbd9ac442f089fad00a4a87336cf21de
Ben Russell <benjaminrussell36@gmail.com>
2020-09-11 10:53:58 -0700

    Add logic to ReusableForm.js

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,48 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function ReusableForm(props){
+  return(
+    <React.Fragment>
+      <Form onSubmit={props.formSubmissionHandler}>
+        <Form.Group>
+          <Form.Control 
+            type="text"
+            name="name"
+            placeholder="Name of Beer" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control 
+            type="text"
+            name="brand"
+            placeholder="Brand Name" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control 
+            type="text"
+            name="price"
+            placeholder="Price of a Pint" />
+        </Form.Group>
+        <Form.Group>
+          <Form.Control 
+            type="text"
+            name="alcoholContent"
+            placeholder="ABV" />
+        </Form.Group>
+        <div style={{padding: 10}}>
+          <Button variant="outline-dark" type='submit'>{props.buttonText}</Button>
+        </div>
+      </Form>
+    </React.Fragment>
+  )
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
