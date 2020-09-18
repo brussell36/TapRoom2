1edf7c1f5d82d64aed22137795bbe8b8defef62a
Ben Russell <benjaminrussell36@gmail.com>
2020-09-18 08:55:00 -0700

    Add test for 'ADD_TAP' reducer, pass test

--- src/__tests__/tap-list-reducer.test.js
+++ src/__tests__/tap-list-reducer.test.js
@@ -1,6 +1,7 @@
 import tapListReducer from './../reducers/tap-list-reducer';
 
 describe('tapListReducer', () => {
+  let action;
   const defaultState = {
     1: {
       name: 'IPA',
@@ -22,5 +23,45 @@ describe('tapListReducer', () => {
 
   test('Should return default state if no action passed into the reducer', () => {
     expect(tapListReducer(defaultState, {type:null})).toEqual(defaultState);
+  });
+
+  test('Should successfully add new tap data to masterTapList', ()=> {
+    const { name, brand, price, alcoholContent, pints, id } = defaultState;
+    action = {
+      type: 'ADD_TAP',
+      name,
+      brand,
+      price,
+      alcoholContent,
+      pints,
+      id
+    };
+    expect(tapListReducer({}, action)).toEqual({
+      [id]: {
+        name,
+        brand,
+        price,
+        alcoholContent,
+        pints,
+        id
+      }
+    });
+  });
+
+  test('Should successfully delete a tap', () => {
+    action = {
+      type: 'DELETE_TAP',
+      id: 1
+    };
+    expect(tapListReducer(defaultState, action)).toEqual({
+      2: {
+        name: 'AltBeer',
+        brand: 'Occidental',
+        price: '$6',
+        alcoholContent: '5.5%',
+        pints: 124,
+        id: 2
+      }
+    });
   });
 });
