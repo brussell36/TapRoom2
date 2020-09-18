fb8f03efe1a94b18d7323d91d1490d02afc82a14
Ben Russell <benjaminrussell36@gmail.com>
2020-09-18 13:46:33 -0700

    Create test for new selectedTapReducer, fail test

---
+++ src/__tests__/reducers/selected-tap-reducer.test.js
@@ -0,0 +1,27 @@
+import selectedTapReducer from './../../reducers/selected-tap-reducer';
+
+describe('selectedTapReducer', () => {
+  let action;
+  const defaultState = {
+    1: {
+      name: 'IPA',
+      brand: 'Pfriem',
+      price: '$6',
+      alcoholContent: '7.2%',
+      pints: 124,
+      id: 1
+    },
+    2: {
+      name: 'AltBeer',
+      brand: 'Occidental',
+      price: '$6',
+      alcoholContent: '5.5%',
+      pints: 124,
+      id: 2
+    }
+  }
+
+  test('Should return null if no tap is selected', () => {
+    expect(selectedTapReducer(null, {type: null})).toEqual(null);
+  });
+});
